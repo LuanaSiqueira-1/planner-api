db17ab882e4a2ce28891c384bf3a3f2357a88add
Luana Siqueira de Sousa <luana.siqueira@ufape.edu.br>
2026-07-17 01:52:13 -0300

    feat(busca): adiciona buscas em tarefas, metas e lembretes

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,44 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/lembretes/proximos', [LembreteController::class, 'proximos']);
 
+    Route::get('/lembretes/ativos', [LembreteController::class, 'ativos']);
+    
+    Route::get(
+    '/lembretes/recorrentes',
+    [LembreteController::class, 'recorrentes']
+);
+    Route::get(
+    '/lembretes/data/{data}',
+    [LembreteController::class, 'buscarPorData']
+);
+    Route::get(
+    '/lembretes/usuario/{id}',
+    [LembreteController::class, 'buscarPorUsuario']
+);
     Route::apiResource('lembretes', LembreteController::class);
+    Route::get(
+    '/metas/status/{status}',
+    [MetaController::class, 'buscarPorStatus']
+);
+    Route::get(
+    '/metas/categoria/{id}',
+    [MetaController::class, 'buscarPorCategoria']
+);
+    Route::get(
+    '/metas/periodo/{periodo}',
+    [MetaController::class, 'buscarPorPeriodo']
+);
+    Route::get(
+    '/metas/usuario/{id}',
+    [MetaController::class, 'buscarPorUsuario']
+);
     Route::apiResource('metas', MetaController::class);
     Route::apiResource('categorias', CategoriaController::class);
+    Route::get('/tarefas/status/{status}', [TarefaController::class, 'buscarPorStatus']);
+    Route::get('/tarefas/categoria/{id}', [TarefaController::class, 'buscarPorCategoria']);
+    Route::get('/tarefas/prioridade/{prioridade}', [TarefaController::class, 'buscarPorPrioridade']);
+    Route::get('/tarefas/data/{data}', [TarefaController::class, 'buscarPorData']);
+    Route::get('/tarefas/turno/{turno}', [TarefaController::class, 'buscarPorTurno']);
+    Route::get('/tarefas/usuario/{id}', [TarefaController::class, 'buscarPorUsuario']);
     Route::apiResource('tarefas', TarefaController::class);
 });
